541037d2b42686d3bfa431f89b418bb4d5c602b1
aamanzi <alnick.amanzi@gmail.com>
2021-10-26 22:26:18 +0200

    fixes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const tecaj = 0.123
+const tecaj = 0.123;
 const kontakti = [
   'Anita',
   'Ante',
@@ -12,9 +12,9 @@ const kontakti = [
   'Luka',
   'Mate',
   'Matea'
-]
+];
 
-let stanjeRacuna = 500
+let stanjeRacuna = 500;
 const transakcije = [
   {
     osoba: 'Ante',
@@ -31,63 +31,63 @@ const transakcije = [
 ];
 
 function dohvatiPoslaneTransakcije() {
-  const dohvaceneTransakcije = []
+  const dohvaceneTransakcije = [];
 
   for (const transakcija of transakcije) {
     if (transakcija.jeLiIzlazna) {
-      dohvaceneTransakcije.push(transakcija)
+      dohvaceneTransakcije.push(transakcija);
     }
   }
 
-  return dohvaceneTransakcije
+  return dohvaceneTransakcije;
 }
 
 function dohvatiPrimljeneTransakcije() {
-  const dohvaceneTransakcije = []
+  const dohvaceneTransakcije = [];
 
   for (const transakcija of transakcije) {
     if (!transakcija.jeLiIzlazna) {
-      dohvaceneTransakcije.push(transakcija)
+      dohvaceneTransakcije.push(transakcija);
     }
   }
 
-  return dohvaceneTransakcije
+  return dohvaceneTransakcije;
 }
 
-function preracunajIznos (iznos, valuta) {
+function preracunajIznos(iznos, valuta) {
   if (valuta === 'hrk') {
-    return iznos * tecaj
+    return iznos * tecaj;
   } else if (valuta === 'dc') {
-    return iznos / tecaj
+    return iznos / tecaj;
   } else {
-    console.log('Krivo pozvana funkcija')
+    window.alert('Krivo pozvana funkcija');
   }
 }
 
-function posaljiDumpCoin (iznos, osoba) {
+function posaljiDumpCoin(iznos, osoba) {
   if (iznos > stanjeRacuna) {
-    return 'Nedovoljno stanje na racunu'
+    return 'Nedovoljno stanje na racunu';
   }
 
-  stanjeRacuna -= iznos
+  stanjeRacuna = stanjeRacuna - iznos;
 
   const novaTransakcija = {
     osoba: osoba,
     iznos: iznos,
     vrijemeIDatum: (new Date()).toLocaleString(),
     jeLiIzlazna: true
-  }
+  };
 
-  transakcije.push(novaTransakcija)
+  transakcije.push(novaTransakcija);
 
-  return 'Uspjesno poslano' + iznos + 'DUMP Coina'
+  return 'Uspjesno poslano' + iznos + 'DUMP Coina';
 }
 
-function zatraziDumpCoin (iznos, osoba) {
-  const hoceLiOsobaPoslat = Math.floor(Math.random() * 2) === 0
+function zatraziDumpCoin(iznos, osoba) {
+  const hoceLiOsobaPoslat = Math.floor(Math.random() * 2) === 0;
 
   if (!hoceLiOsobaPoslat) {
-    return 'Osoba je odbila poslati' + iznos + 'DUMP Coina'
+    return 'Osoba je odbila poslati' + iznos + 'DUMP Coina';
   }
 
   const novaTransakcija = {
@@ -95,9 +95,9 @@ function zatraziDumpCoin (iznos, osoba) {
     iznos: iznos,
     vrijemeIDatum: (new Date()).toLocaleString(),
     jeLiIzlazna: false
-  }
+  };
 
-  transakcije.push(novaTransakcija)
+  transakcije.push(novaTransakcija);
 
-  return 'Uspjesno primljeno' + iznos + 'DUMP Coina'
+  return 'Uspjesno primljeno' + iznos + 'DUMP Coina';
 }
